e80f2b98604ef074b72c67d32b85d14694de1364
dotGrimmz <rakeemxng@gmail.com>
2022-08-15 09:36:25 -0400

    updated readme

--- src/components/layout/Layout.styles.ts
+++ src/components/layout/Layout.styles.ts
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
   display: flex;
@@ -6,35 +6,4 @@ export const LayoutContainer = styled.div`
   flex-direction: column;
   background-color: green;
   align-items: center;
-`;
-
-// diff component
-
-const growDown = keyframes`
-  0% {
-    transform: scaleY(0)
-  }
-  80% {
-    transform: scaleY(1.1)
-  }
-  100% {
-    transform: scaleY(1)
-  }`;
-
-export const UserAvailabilityContainer = styled.div`
-  display: flex;
-  outline: 1.5px solid black;
-  width: 200px;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.19);
-  background-color: white;
-  align-self: flex-start;
-  margin-top: 10px;
-  margin-left: 20px;
-  border-radius: 5px;
-  padding: 10px;
-  gap: 6px;
-  position: absolute;
-  top: 100px;
-  animation: ${growDown} 300ms ease-in-out forwards;
-  transform-origin: top center;
 `;
